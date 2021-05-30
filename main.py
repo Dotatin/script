394d64e8973c12993c5bf2c17afee6a47933ae48
Dotatin <85061128+Dotatin@users.noreply.github.com>
2021-05-30 14:18:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,20 +6,9 @@ import dlink
 
 
 print('Select company:')
-print('1.Proline\n2.Delta\n3.Rusanovka\n4.Sunline\n5.Bereznyaki')
 print('Your Answer:')
 company = int(raw_input())
 
-
-if company == 1 or company == 2 or company == 5:
-	user = 'swadmin'
-	password =',hjv,thu'
-elif company == 3:
-	user = 'andrey'
-	password ='sk'
-elif company == 4:
-	user = 'admin'
-	password ='SKor314O'
 
 print('\nSelect switch:')
 print('1.D-link 3526\n2.D-link 3028\n3.D-link 3200-28F\n4.Edge-Core')
